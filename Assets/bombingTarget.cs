d613462bb798d3713f8f0aa88881d3b20f65ce24
Stevo <blazeje@gmail.com>
2014-01-26 13:04:53 +0100

    shooting sounds for enemies

---
+++ Assets/bombingTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bombingTarget : MonoBehaviour {
+
+	GameObject player;
+	Vector3 distanceSqr;
+	public AudioClip siren;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("Something has entered this zone.");    
+	} 
+}
