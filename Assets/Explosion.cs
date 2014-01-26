b0a66380b7187f1ca9ab2a7b0f9a319b1b8f8dcc
Arkadiusz Kwaśny <arkadiusz.kwasny@gmail.com>
2014-01-26 10:50:10 +0100

    added bombarding

---
+++ Assets/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	float count;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count += Time.deltaTime;
+		if(count > 1)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
