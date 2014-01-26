288e3a35d11c24a496af5b48425f4b77f1e885de
Stevo <blazeje@gmail.com>
2014-01-26 12:29:58 +0100

    add ducks quacking in the background from time to time`

---
+++ Assets/backgroundSounds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundSounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audio.isPlaying && (Random.value > 0.97f))
+			audio.Play();
+	}
+}
